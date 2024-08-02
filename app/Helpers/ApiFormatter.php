cddb25b49e2a69d12de7dd94cdb37ea948e41736
ilhamlutfi <ilhamlutfi153@gmail.com>
2024-08-02 08:42:25 +0700

    add api response formatter

---
+++ app/Helpers/ApiFormatter.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Helpers;
+
+/**
+ * Format api response.
+ */
+class ApiFormatter
+{
+    /**
+     * API Response
+     *
+     * @var array
+     */
+    protected static $response = [
+        'meta' => [
+            'code' => 200,
+            'status' => 'success',
+            'message' => null,
+        ],
+        'result' => null,
+    ];
+
+    /**
+     * Give success response.
+     */
+    public static function success($data = null, $message = null, $code = 200)
+    {
+        self::$response['meta']['message'] = $message;
+        self::$response['meta']['code'] = $code;
+        self::$response['data'] = $data;
+
+        return response()->json(self::$response, self::$response['meta']['code']);
+    }
+
+    /**
+     * Give error response.
+     */
+    public static function error($data = null, $message = null, $code = 400)
+    {
+        self::$response['meta']['status'] = 'error';
+        self::$response['meta']['code'] = $code;
+        self::$response['meta']['message'] = $message;
+        self::$response['data'] = $data;
+
+        return response()->json(self::$response, self::$response['meta']['code']);
+    }
+}
